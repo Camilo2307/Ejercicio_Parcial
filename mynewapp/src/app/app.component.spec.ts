96c278b134803264c426e01f3bca7046f7d9aec9
Camilo_Peña <cc.pena2@uniandes.edu.co>
2023-11-05 19:32:05 -0500

    Se envian test

--- mynewapp/src/app/app.component.spec.ts
+++ mynewapp/src/app/app.component.spec.ts
@@ -1,12 +1,21 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { PlantaComponent } from './planta/planta.component';
+
 
 describe('AppComponent', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [RouterTestingModule],
-    declarations: [AppComponent]
-  }));
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule, HttpClientModule
+      ],
+      declarations: [
+        AppComponent,PlantaComponent
+      ],
+    }).compileComponents();
+  });
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -20,10 +29,4 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('mynewapp');
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('mynewapp app is running!');
-  });
 });
